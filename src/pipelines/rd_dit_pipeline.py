c48c57d3f6b2299bf34b5069a1c7b97fe729c40e
Jingkai Zhou <1252693592@qq.com>
2025-05-21 11:27:19 +0800

    Update rd_dit_pipeline.py

--- src/pipelines/rd_dit_pipeline.py
+++ src/pipelines/rd_dit_pipeline.py
@@ -50,45 +50,29 @@ EXAMPLE_DOC_STRING = """
     Examples:
         ```python
         >>> import torch
-        >>> import numpy as np
-        >>> from diffusers import AutoencoderKLWan, WanImageToVideoPipeline
-        >>> from diffusers.utils import export_to_video, load_image
+        >>> from diffusers import AutoencoderKLWan
         >>> from transformers import CLIPVisionModel
+        >>> from src.pipelines.rd_dit_pipeline import RealisDanceDiTPipeline
 
-        >>> # Available models: Wan-AI/Wan2.1-I2V-14B-480P-Diffusers, Wan-AI/Wan2.1-I2V-14B-720P-Diffusers
-        >>> model_id = "Wan-AI/Wan2.1-I2V-14B-480P-Diffusers"
+        >>> model_id = "theFoxofSky/RealisDance-DiT"
         >>> image_encoder = CLIPVisionModel.from_pretrained(
         ...     model_id, subfolder="image_encoder", torch_dtype=torch.float32
         ... )
         >>> vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
-        >>> pipe = WanImageToVideoPipeline.from_pretrained(
+        >>> pipe = RealisDanceDiTPipeline.from_pretrained(
         ...     model_id, vae=vae, image_encoder=image_encoder, torch_dtype=torch.bfloat16
         ... )
-        >>> pipe.to("cuda")
-
-        >>> image = load_image(
-        ...     "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/astronaut.jpg"
-        ... )
-        >>> max_area = 480 * 832
-        >>> aspect_ratio = image.height / image.width
-        >>> mod_value = pipe.vae_scale_factor_spatial * pipe.transformer.config.patch_size[1]
-        >>> height = round(np.sqrt(max_area * aspect_ratio)) // mod_value * mod_value
-        >>> width = round(np.sqrt(max_area / aspect_ratio)) // mod_value * mod_value
-        >>> image = image.resize((width, height))
-        >>> prompt = (
-        ...     "An astronaut hatching from an egg, on the surface of the moon, the darkness and depth of space realised in "
-        ...     "the background. High quality, ultrarealistic detail and breath-taking movie-like camera shot."
-        ... )
-        >>> negative_prompt = "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards"
-
+        >>> pipe.enable_model_cpu_offload()
+        
+        >>> # prepare ref_image, smpl, hamer, all of them are in shape B C F H W, in range [-1, 1]
+        
+        >>> max_res = 768*768
         >>> output = pipe(
-        ...     image=image,
+        ...     image=ref_image,
+        ...     smpl=smpl,
+        ...     hamer=hamer,
         ...     prompt=prompt,
-        ...     negative_prompt=negative_prompt,
-        ...     height=height,
-        ...     width=width,
-        ...     num_frames=81,
-        ...     guidance_scale=5.0,
+        ...     max_resolution=max_res,
         ... ).frames[0]
         >>> export_to_video(output, "output.mp4", fps=16)
         ```
